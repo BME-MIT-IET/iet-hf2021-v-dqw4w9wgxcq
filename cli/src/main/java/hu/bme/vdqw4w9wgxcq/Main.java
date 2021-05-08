e0ed5334139f361c258b51739d3c07620585c2b2
Adrián Robotka <robotka.adrian@gmail.com>
2021-05-08 18:28:21 +0200

    Prettify CLI tool

--- cli/src/main/java/hu/bme/vdqw4w9wgxcq/Main.java
+++ cli/src/main/java/hu/bme/vdqw4w9wgxcq/Main.java
@@ -1,37 +1,51 @@
 package hu.bme.vdqw4w9wgxcq;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.Writer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import tr.com.srdc.ontmalizer.XML2OWLMapper;
 import tr.com.srdc.ontmalizer.XSD2OWLMapper;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 class Main {
+    /**
+     * Usage: cli-tool <schema> <in> <out>
+     *
+     * @param args <schema> <in> <out>
+     */
     public static void main(String[] args) {
-        // cli-tool <schema> <in> <out>
+        checkArgs(args);
+
+        File xsdFile = new File(args[0]);
+        File xmlFile = new File(args[1]);
+        File n3File = new File(args[2]);
+
         // This part converts XML schema to OWL ontology.
-        XSD2OWLMapper mapping = new XSD2OWLMapper(new File(args[1]));
+        XSD2OWLMapper mapping = new XSD2OWLMapper(xsdFile);
         mapping.setObjectPropPrefix("");
         mapping.setDataTypePropPrefix("");
         mapping.convertXSD2OWL();
 
         // This part converts XML instance to RDF data model.
-        XML2OWLMapper generator = new XML2OWLMapper(new File(args[2]), mapping);
+        XML2OWLMapper generator = new XML2OWLMapper(xmlFile, mapping);
         generator.convertXML2OWL();
 
         // This part prints the RDF data model to the specified file.
         try {
-            File f = new File(args[3]);
-            f.getParentFile().mkdirs();
-            FileOutputStream fout = new FileOutputStream(f);
+            n3File.getParentFile().mkdirs();
+
+            FileOutputStream fout = new FileOutputStream(n3File);
             generator.writeModel(fout, "N3");
             fout.close();
-
         } catch (Exception e) {
-            //LOGGER.error("{}", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkArgs(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Incorrect arguments");
+            System.err.println("Usage: cli-tool <schema> <in> <out>");
+            System.exit(1);
         }
     }
 }
